595d62ac3a9c69465b196054de3b14be9dd809e4
Toled <itstoled@gmail.com>
2020-03-05 09:30:51 +0100

    basic routing added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 
@@ -8,7 +9,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
       </div>
     );
   }
