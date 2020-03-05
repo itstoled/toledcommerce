b382a793aa6379aba7bf6164036c50b391c24bc0
Toled <itstoled@gmail.com>
2020-03-05 10:44:13 +0100

    shop page styled without hover

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-
+import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
 
 class App extends React.Component {
@@ -11,6 +11,7 @@ class App extends React.Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
         </Switch>
       </div>
     );
